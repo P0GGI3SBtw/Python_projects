90e6ecab75fb3d6187757019f81d66bbe47b7d62
Szymon <77151200+P0GGI3SBtw@users.noreply.github.com>
2022-04-18 21:02:02 +0200

    done

--- functions/operators.py
+++ functions/operators.py
@@ -5,14 +5,14 @@ def show(var, integer1, integer2, label_text):
     result = 0
     if var.get() == 1:
         result = int(integer1.get()) + int(integer2.get())
-    if var.get() == 2:
+    elif var.get() == 2:
         result = int(integer1.get()) - int(integer2.get())
-    if var.get() == 3:
+    elif var.get() == 3:
         result = int(integer1.get()) * int(integer2.get())
-    if var.get() == 4:
+    elif var.get() == 4:
         if int(integer2.get()) == 0:
             result = "You can't divide by zero"
         else:
-            result = int(integer1.get()) / int(integer2.get())
+            result = float(integer1.get()) / float(integer2.get())
 
-    label_text.config(text=f'Result of action is {int(result)}')
+    label_text.config(text=f'Result of action is {result}')

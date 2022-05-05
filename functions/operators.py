ec3b8d7e144aa19d55933f0d5c88f22d8277593b
Szymon <77151200+P0GGI3SBtw@users.noreply.github.com>
2022-05-05 18:14:55 +0200

    done

--- functions/operators.py
+++ functions/operators.py
@@ -3,16 +3,19 @@
 
 def show(var, integer1, integer2, label_text):
     result = 0
-    if var.get() == 1:
-        result = int(integer1.get()) + int(integer2.get())
-    elif var.get() == 2:
-        result = int(integer1.get()) - int(integer2.get())
-    elif var.get() == 3:
-        result = int(integer1.get()) * int(integer2.get())
-    elif var.get() == 4:
-        if int(integer2.get()) == 0:
-            result = "You can't divide by zero"
-        else:
-            result = float(integer1.get()) / float(integer2.get())
+    match var.get():
+        case 1:
+            result = int(integer1.get()) + int(integer2.get())
+        case 2:
+            result = int(integer1.get()) - int(integer2.get())
+        case 3:
+            result = int(integer1.get()) * int(integer2.get())
+        case 4:
+            if int(integer2.get()) == 0:
+                result = "You can't divide by zero"
+            else:
+                result = float(integer1.get()) / float(integer2.get())
+        case 5:
+            result = int(integer1.get()) ** int(integer2.get())
 
-    label_text.config(text=f'Result of action is {result}')
+    label_text.config(text=f'Result of action is {round(result, 2)}')
